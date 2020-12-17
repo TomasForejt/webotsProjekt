0c6e7153972e872d1579f45158c58ad70884851a
TomasForejt <50369118+TomasForejt@users.noreply.github.com>
2020-12-17 16:31:15 +0100

    gate

--- controllers/robot_controller/robot_controller.m
+++ controllers/robot_controller/robot_controller.m
@@ -19,13 +19,11 @@ motorPZ = wb_robot_get_device('motorPZ');
 irP = wb_robot_get_device('irP');
 irL = wb_robot_get_device('irL');
 
-kloub1 = wb_robot_get_device('kloub1');
-kloub2 = wb_robot_get_device('kloub2');
-kloub3 = wb_robot_get_device('kloub3');
-ruka = wb_robot_get_device('ruka');
+dst = wb_robot_get_device('dst');
 
 wb_distance_sensor_enable(irP,TIME_STEP);
 wb_distance_sensor_enable(irL,TIME_STEP);
+wb_distance_sensor_enable(dst,TIME_STEP);
 
 wb_motor_set_position(motorLP, inf);
 wb_motor_set_position(motorLZ, inf);
@@ -50,8 +48,10 @@ wb_motor_set_velocity(motorPZ, 2);
 % and leave the loop when Webots signals the termination
 %
 while wb_robot_step(TIME_STEP) ~= -1
-irP_value = wb_distance_sensor_get_value(irP)
+
+irP_value = wb_distance_sensor_get_value(irP);
 irL_value = wb_distance_sensor_get_value(irL);
+dst_value = wb_distance_sensor_get_value(dst);
 
 if irL_value > 500
 
@@ -74,6 +74,15 @@ wb_motor_set_velocity(motorLP, -2);
 wb_motor_set_velocity(motorLZ, -2);
 wb_motor_set_velocity(motorPP, 2);
 wb_motor_set_velocity(motorPZ, 2);
+
+end
+
+if dst_value < 60
+
+wb_motor_set_velocity(motorLP, 0);
+wb_motor_set_velocity(motorLZ, 0);
+wb_motor_set_velocity(motorPP, 0);
+wb_motor_set_velocity(motorPZ, 0);
 
 end
 
